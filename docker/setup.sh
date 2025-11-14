de67c8c361c8edb6ba8f4f9a62415cad794c7ad0
Donghee Park <dongheepark@gmail.com>
2025-11-14 11:12:17 +0900

    Update setup.sh for Index.dat and Patient

--- docker/setup.sh
+++ docker/setup.sh
@@ -2,4 +2,8 @@
 
 cd "$(dirname "$0")"
 
+# Create Index.dat to prepare frontend 
+touch ../../Index.dat
+mkdir -p ../../Patient
+
 docker compose up -d --build
--- docker/simulation.sh
+++ docker/simulation.sh
@@ -6,7 +6,7 @@ cd "$(dirname "$0")"
 # TODO: use shell script instead of backend API
 curl -X POST http://localhost:5000/api/xml/copy
 
-docker compose up -d
+docker compose up -d --force-recreate
 
 ./stop.sh simulation
 
@@ -14,5 +14,4 @@ CONTAINER_NAME=wearable_robot_mujoco
 SIMULATION_NODE=simulation_node
 
 echo "Starting $SIMULATION_NODE in container $CONTAINER_NAME"
-#docker exec -it -d $CONTAINER_NAME bash -c "source /opt/ros/humble/setup.bash && colcon build --symlink-install && source /wearable_ws/install/setup.sh && ros2 run wearable_robot_mujoco $SIMULATION_NODE"
 docker exec -it $CONTAINER_NAME bash -c "source /opt/ros/humble/setup.bash && colcon build --symlink-install && source /wearable_ws/install/setup.sh && ros2 run wearable_robot_mujoco $SIMULATION_NODE"

725cba553836076a5f82b2fad8dd1ca492604cc3
Donghee Park <dongheepark@gmail.com>
2025-12-26 14:40:34 +0900

    Update to launch the mujoco at right of screen

--- docker/simulation.sh
+++ docker/simulation.sh
@@ -14,4 +14,23 @@ CONTAINER_NAME=wearable_robot_mujoco
 SIMULATION_NODE=simulation_node
 
 echo "Starting $SIMULATION_NODE in container $CONTAINER_NAME"
-docker exec -it $CONTAINER_NAME bash -c "source /opt/ros/humble/setup.bash && colcon build --symlink-install && source /wearable_ws/install/setup.sh && ros2 run wearable_robot_mujoco $SIMULATION_NODE"
+docker exec -it -d $CONTAINER_NAME bash -c "source /opt/ros/humble/setup.bash && colcon build --symlink-install && source /wearable_ws/install/setup.sh && ros2 run wearable_robot_mujoco $SIMULATION_NODE"
+
+# repeatly check if the simulation node is running
+while true; do
+  wmctrl -l | grep "MuJoCo : Elbow joint with single muscle" > /dev/null
+  if [ $? -eq 0 ]; then
+    echo "$SIMULATION_NODE is running"
+    break
+  else
+    echo "Waiting for $SIMULATION_NODE to start..."
+    sleep 0.01
+  fi
+done
+
+DISPLAY_SIZE=$(xdpyinfo | grep 'dimensions:' | awk '{print $2}')
+WIDTH=$(echo $DISPLAY_SIZE | cut -d'x' -f1)
+HEIGHT=$(echo $DISPLAY_SIZE | cut -d'x' -f2)
+WIDTH_TWO_THIRDS=$((WIDTH * 2 / 3))
+WIDTH_ONE_THIRD=$((WIDTH / 3))
+wmctrl -r "MuJoCo : Elbow joint with single muscle" -e "0,$WIDTH_TWO_THIRDS,0,$WIDTH_ONE_THIRD,$HEIGHT"

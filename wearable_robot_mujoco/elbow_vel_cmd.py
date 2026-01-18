6e691d6418c015fea951f10aa3e4186a03f2e30c
Donghee Park <dongheepark@gmail.com>
2026-01-18 16:51:08 +0900

    Update Upperlimb_1DOF class for task 2

--- wearable_robot_mujoco/elbow_vel_cmd.py
+++ wearable_robot_mujoco/elbow_vel_cmd.py
@@ -12,6 +12,7 @@ class Upperlimb_1DOF:
         self.rep_count = 0
         self.freq = 60
         self.velocity_cmd = 0.0
+        self.DELTA_TIME = 0.02  # Control period (20ms)
 
     def init(self, rep_count=20, freq=60):
         """Initialize the upperlimb control parameters"""
@@ -44,6 +45,42 @@ class Upperlimb_1DOF:
         """Get the previous sensor velocity"""
         if self.node:
             return self.node.previous_velocity
+        return 0.0
+
+    def set_previous_velocity(self, velocity):
+        """Set the previous sensor velocity"""
+        if self.node:
+            self.node.previous_velocity = velocity
+
+    def get_force(self):
+        """Get the current sensor force"""
+        if self.node:
+            return self.node.sensor_force
+        return 0.0
+
+    def get_position(self):
+        """Get the current sensor position"""
+        if self.node:
+            return self.node.sensor_position
+        return 0.0
+
+    def get_moment(self):
+        """Get the moment of inertia (관성 계수)"""
+        return 0.0  # Default value, can be configured
+
+    def get_spring(self):
+        """Get the spring coefficient (spring 계수)"""
+        return 0.0  # Default value, can be configured
+
+    def get_damping(self):
+        """Get the damping coefficient (damping 계수)"""
+        return 100.0  # Default value, same as in elbow_vel_cmd control_logic
+
+    @property
+    def target_th(self):
+        """Get the target angle in radians"""
+        if self.node:
+            return self.node.target_th
         return 0.0
 
 
--- wearable_robot_mujoco/task.py
+++ wearable_robot_mujoco/task.py
@@ -4,8 +4,23 @@ def setup():
     upperlimb.init(rep_count=20, freq=60)
 
 def loop():
-    # task1
+    # task 1
+    #  if upperlimb.get_target_angle() > 90:
+    #      upperlimb.set_velocity(-1.2)
+    #  else:
+    #      upperlimb.set_velocity(1.2)
+
+    # task 2
+    delta_velocity = upperlimb.get_velocity() - upperlimb.get_previous_velocity()
+    acceleration = delta_velocity / upperlimb.DELTA_TIME
+    acceleration = max(min(acceleration, 500.0), -500.0)
+    delta_force = upperlimb.get_force() * 1000 / 9.8   # dimension change
+    m = upperlimb.get_moment() # moment of inertia 관성 계수
+    k = upperlimb.get_spring() # spring 계수
+    c = upperlimb.get_damping() # damping 계수
+
     if upperlimb.get_target_angle() > 90:
-        upperlimb.set_velocity(-1.2)
+        velocity = -0.6 + (delta_force - m * acceleration - k * (upperlimb.get_position() - upperlimb.get_target_angle())) / c # flexion
     else:
-        upperlimb.set_velocity(1.2)
+        velocity = 5.2  # extension
+    upperlimb.set_velocity(velocity)

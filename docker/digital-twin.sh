f4c456698a49c09efd4a136a4b525546e2398079
Donghee Park <dongheepark@gmail.com>
2026-07-07 13:18:06 +0900

    Update toz simulator

--- docker/digital-twin.sh
+++ docker/digital-twin.sh
@@ -5,4 +5,8 @@ sleep 0.5
 
 cd "$(dirname "$0")"
 
+patient_name=$(cat ../../Index.dat| head -n 1)
+echo "Patient name: $patient_name"
+cp ../../Patient/$patient_name/result.csv ../../Wearable_Linux/Wearable/CSV/elbow_joint_quaternion_extended_20260114_171949.csv
+
 ../../Wearable_Linux/Wearable.sh
